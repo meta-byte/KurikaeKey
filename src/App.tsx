682f4eeec4d420db5ee83ab5d1ccb5718cb2a85d
Garrett <45606335+meta-byte@users.noreply.github.com>
2024-04-02 09:34:28 -0600

    colorize matches and misses

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,51 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import "./index.css";
 
 function App() {
   const [key, setKey] = useState<string>("Key");
-  const [word] = useState<string>("test");
+  const [word] = useState<string>(
+    "the quick brown fox jumped over the lazy dog"
+  );
   const [keyIndex, setKeyIndex] = useState<number>(-1);
-
+  const [matchingIndices, setMatchingIndices] = useState<number[]>([]);
+  const [missingIndices, setMissingIndices] = useState<number[]>([]);
   const split: string[] = word.split("");
+
   useEffect(() => {
     if (keyIndex <= split.length) {
       console.log(split[keyIndex]);
       if (split[keyIndex] == key) {
-        console.log("pass");
+        setMatchingIndices((prevMatching) => [...prevMatching, keyIndex])
       } else {
-        console.log("miss");
+        setMissingIndices((prevMissing) => [...prevMissing, keyIndex])
       }
     }
   }, [keyIndex]);
 
+  useEffect(() => {}, []);
   onkeydown = (e) => {
     setKey(e.key);
     setKeyIndex((prevIndex) => prevIndex + 1);
   };
 
+  const coloredString = useMemo(() => {
+    console.log(matchingIndices)
+    return split.map((char, index) => {
+      const isMatch = matchingIndices.includes(index);
+      const isMiss = missingIndices.includes(index);
+      return (
+        <span key={index} className={isMatch ? "black" : isMiss ? "red" : ""}>
+          {char}
+        </span>
+      );
+    });
+  }, [word, matchingIndices, missingIndices]);
+
   return (
     <div className="main">
-      <h1>{word}</h1>
+      <div className="word-box">
+        <h1>{coloredString}</h1>
+      </div>
       <h1>{key}</h1>
     </div>
   );
